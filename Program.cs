df89a4d4dd3bdecf03bc1e5fa7e2d41862f0527e
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-16 13:37:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checking_if_two_intergers_sum_to_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two integers:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            bool result = (num1 == 30 || num2 == 30) || (num1 + num2 == 30);
+
+            Console.WriteLine("Result: " + result);
+
+        }
+    }
+}
